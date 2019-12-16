0408e86a23f29662c99d353fede8226346e78547
Noble Millie <noble.millie@gmail.com>
2019-12-16 02:16:07 -0800

    combining area radial

--- linear-gradient/script.js
+++ linear-gradient/script.js
@@ -1,22 +1,21 @@
 //////////////////// Set up and initiate svg containers ///////////
 var margin = {
 	top: 50,
-	right: 20,
+	right: 70,
 	bottom: 80,
-	left: 20
+	left: 0
 };
-var width = (window.innerWidth - margin.left - margin.right - 10) / 2;
-var height = window.innerHeight - margin.top - margin.bottom - 40;
+var width = (window.innerWidth - margin.left - margin.right - 10) / 1.05;
+var height = window.innerHeight - margin.top - margin.bottom - 30;
 
 //SVG container
 var svg = d3
 	.select('#weatherRadial')
 	.append('svg')
-	.attr('width', width + margin.left + margin.right - 10)
-	.attr('height', height + margin.top + margin.bottom - 50)
-	// .style('background-color', 'snow')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
 	.style('background-color', 'rgba(11, 11, 11, 0.7)')
-	.style('border', '3px solid cyan')
+	// .style('border', '1px solid cyan')
 	.append('g')
 	.attr(
 		'transform',
@@ -54,6 +53,14 @@ d3.json('historical.json', function(w) {
 
 	return squawData;
 });
+
+// convert between celsius and fahrenheit
+function celsiusToFahrenheit(c) {
+	const w = 1.8;
+	const b = 32;
+	const f = c * w + b;
+	return f;
+}
 
 //////////////////////////// Create scales /////////////////////////
 //Parses a string into a date
@@ -434,7 +441,7 @@ barWrapper
 		return colorScale(d.mean_temp);
 	});
 
-//////////////// Create the gradient for the legend ////////////////
+//////////////// Create the gradient for the legend ///////////////
 //Extra scale since the color scale is interpolated
 var tempScale = d3.scale
 	.linear()
@@ -521,3 +528,160 @@ legendsvg
 	.attr('class', 'axis')
 	.attr('transform', 'translate(0,' + 10 + ')')
 	.call(xAxis);
+
+// Data
+let snowData = [];
+const days = d3.timeDay.range(new Date(2019, 0, 1), new Date(2019, 11, 31));
+
+// Scales
+const xSnowScale = d3
+	.scaleTime()
+	.domain(d3.extent(days))
+	.range([0, Math.PI * 2]); // range is the diameter of the circle
+
+const ySnowScale = d3.scaleRadial().domain([0, 60]);
+
+// Generators
+const areaGenerator = d3
+	.areaRadial()
+	.angle(d => xSnowScale(d.date))
+	.innerRadius(d => ySnowScale(d.v0))
+	.outerRadius(d => ySnowScale(d.v1))
+	.curve(d3.curveBasis);
+
+// Elements
+// const svg = d3.select('body').append('svg');
+const g = svg.append('g');
+
+// const xAxis = g.append('g').attr('class', 'axis');
+
+// const xAxisTicks = xAxis
+// 	.selectAll('.tick')
+// 	.data(d3.timeMonth.every(1).range(...d3.extent(days)))
+// 	.enter()
+// 	.append('g')
+// 	.attr('class', 'tick');
+
+// xAxisTicks
+// 	.append('text')
+// 	.attr('dx', 65)
+// 	.attr('dy', -15)
+// 	.attr('transform', 'rotate(15)')
+// 	.text(d => `${d3.timeFormat('%b')(d)}.`);
+
+// xAxisTicks.append('line').attr('y2', -10);
+
+// const yAxis = g.append('g').attr('class', 'axis');
+
+// const yAxisTicks = yAxis
+// 	.selectAll('.tick')
+// 	.data(ySnowScale.ticks(5).slice(1))
+// 	.enter()
+// 	.append('g')
+// 	.attr('class', 'tick')
+// 	.style('opacity', 0.4);
+
+// const yAxisCircles = yAxisTicks.append('circle');
+
+// const yAxisTextTop = yAxisTicks
+// 	.append('text')
+// 	.attr('dy', -5)
+// 	.text(d => d);
+
+// const yAxisTextBottom = yAxisTicks
+// 	.append('text')
+// 	.attr('dy', 12)
+// 	.text(d => d);
+
+// Updater
+const duration = 1750;
+makeData();
+// redraw();
+// onresize = _ => redraw(true);
+//       d3.interval(_ => {
+//         makeData();
+//         redraw();
+//       }, duration * 12);
+
+// function redraw(resizing) {
+// 	const diameter = Math.min(innerWidth, innerHeight);
+// 	width = diameter - margin.left - margin.right;
+// 	height = diameter - margin.top - margin.bottom;
+
+// 	ySnowScale.range([0, height / 2]);
+
+// 	// svg
+// 	// 	.attr('width', width + margin.left + margin.right)
+// 	// 	.attr('height', height + margin.top + margin.bottom);
+
+// 	g.attr(
+// 		'transform',
+// 		`translate(${margin.left + width / 2}, ${margin.top + height / 2})`
+// 	);
+
+// 	// xAxisTicks.attr('transform', (d, i, e) => {
+// 	// 	const point = [width / 2, 0];
+// 	// 	const angle = (i / e.length) * 360;
+// 	// 	const rotated = geometric.pointRotate(point, 270 + angle);
+// 	// 	return `translate(${rotated}) rotate(${angle})`;
+// 	// });
+
+// 	// yAxisCircles.attr('r', d => ySnowScale(d));
+
+// 	// yAxisTextTop.attr('y', d => ySnowScale(d));
+
+// 	// yAxisTextBottom.attr('y', d => -ySnowScale(d));
+
+// 	// General update pattern for the area, whose data changes
+// 	// const area = g.selectAll('.area').data([snowData]);
+// 	const area = g
+// 		.selectAll('.area')
+// 		.data([snowData])
+// 		.attr('d', areaGenerator);
+
+// 	// if (resizing) {
+// 	// 	area.attr('d', areaGenerator);
+// 	// } else {
+// 	// 	area
+// 	// 		.transition()
+// 	// 		.duration(duration)
+// 	// 		.attr('d', areaGenerator);
+// 	// }
+
+// 	area
+// 		.enter()
+// 		.append('path')
+// 		.attr('class', 'area')
+// 		.attr('d', areaGenerator)
+// 		.style('opacity', 0)
+// 		.transition()
+// 		.duration(duration)
+// 		.style('opacity', 1);
+// }
+
+// Functions for generating random data
+function makeData() {
+	v0 = randBetween(0, 5);
+	v1 = randBetween(10, 30);
+
+	snowData = days.map(date => {
+		v1 = Math.min(v1 + random([-1.7, 2]), 50);
+		v0 = 5;
+		//           v0 = Math.min(Math.max(v0 + random([-1, 1]), 1), v1 - 5)
+		const obj = {
+			date,
+			v1,
+			v0
+		};
+		console.log('areaObj: ', obj);
+		return obj;
+	});
+}
+
+function randBetween(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function random(arr) {
+	return arr[randBetween(0, arr.length - 1)];
+}
